6256886774bed6524e36a7beeab6a29b9ecad54e
trinachuamin <23017106@myrp.edu.sg>
2025-02-06 14:25:44 +0800

    recipecon

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -5,16 +5,16 @@ export const AuthContext = createContext()
 export const authReducer = (state, action) => {
     switch (action.type) {
         case 'LOGIN':
-        return { user: action.payload }
+            return { user: action.payload }
         case 'LOGOUT':
-        return { user: null }
+            return { user: null }
         default:
-        return state
+            return state
     }
-    }
+}
 
-    export const AuthContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(authReducer, { 
+export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(authReducer, {
         user: null
     })
 
@@ -22,15 +22,15 @@ export const authReducer = (state, action) => {
         const user = JSON.parse(localStorage.getItem('user'))
 
         if (user) {
-        dispatch({ type: 'LOGIN', payload: user }) 
+            dispatch({ type: 'LOGIN', payload: user })
         }
     }, [])
 
     console.log('AuthContext state:', state)
-    
+
     return (
         <AuthContext.Provider value={{ ...state, dispatch }}>
-        { children }
+            {children}
         </AuthContext.Provider>
     )
 
